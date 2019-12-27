cde484bf458c384cbef08dfc930c8e68e14c029f
cho-jinyoung <chojinyoung22@naver.com>
2019-12-28 01:51:03 +0900

    1

---
+++ student_info.h
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS_
+#include <stdio.h> 
+#include <malloc.h> 
+#include <string.h> 
+
+typedef struct _Node { 
+	unsigned char student_id[20]; 
+	unsigned char department_id; 
+	unsigned char department_name[40]; 
+	unsigned char grade; 
+	unsigned char name[80]; 
+	unsigned char birthday[20]; 
+	
+	struct _Node *next;
+	struct _Node *prev;
+} Node;
+
+
